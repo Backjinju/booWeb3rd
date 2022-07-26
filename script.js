cb7b4094f8de08ed58cb3ee5e77c4dbc3628ea74
Backjiju <100hjhj@naver.com>
2022-07-26 10:24:08 +0900

    0726오전 백진주

--- script.js
+++ script.js
@@ -895,33 +895,6 @@ SGslides.addEventListener('mouseleave',function(){
 ///////////////////////////////////////
 //////////////////진주//////////////////
 
-// $(document).ready(function(){
-
-//     var navHeight = $(".genre_mid_slide").height(); 
-//     //navHeight 의 높이를 구하기
-
-//     $(".genre_mid_slide_box").hide();
-//     //스크롤시 나타날 객체 미리 숨기기
-
-//     $(window).scroll(function(){  // 윈도우 스크롤 기능 작동
-//         var rollIt = $(this).scrollTop() >= navHeight; 
-// 스크롤의 정도가 navHeight 의 값을 넘었을 때 == 윈도우 스크롤의 값이 navHeight 의 높이와 같거나 크다
-
-/*
-scrollTop 은 윈도우에서 스크롤의 위치가 가장 상위에 있다는 의미로 해석
-스크롤의 위치가 화면 아래일수록 == scrollTop 의 값이 커짐
-*/
-
-//     if(rollIt){ 
-// 		//윈도우 스크롤 기능의 값이 navHeight 의 높이와 같거나 크면
-//             $(".genre_mid_slide_box").show().css({"position":"fixed"});
-//         }
-//         else{
-//             $(".genre_mid_slide_box").hide();
-//         }
-//     });
-    
-// });
 
 
 //////////////////진주끝/////////////////////
